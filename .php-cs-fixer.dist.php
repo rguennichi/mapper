301e05481963c842817abb660f727a9a4125d43b
Radhi Guennichi <radhi@guennichi.com>
2022-09-12 12:50:25 +0200

    add .php-cs-fixer file

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'native_function_invocation' => true,
+        'native_constant_invocation' => true,
+        'ordered_imports' => true,
+        'declare_strict_types' => true,
+        'single_import_per_statement' => false,
+        'concat_space' => ['spacing' => 'one'],
+        'phpdoc_align' => ['align' => 'left'],
+        'trailing_comma_in_multiline' => ['elements' => ['arrays', 'arguments', 'parameters', 'match']],
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in(__DIR__)
+            ->name('*.php')
+    )
+;
